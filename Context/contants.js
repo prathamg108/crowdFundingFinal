7a47512317e0a18547fed5a217c2fd155bfa59a5
Prathamg136 <pratham.gandhi20@pccoepune.org>
2024-04-22 01:09:57 +0530

    adding final touches

---
+++ Context/contants.js
@@ -0,0 +1,8 @@
+import crowdFunding from "./CrowdFunding.json";
+
+//NFT MARKETPLACE
+//LOCAL HOST TESTING HARDHAT
+// export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// POLYGON MUMBAI
+export const CrowdFundingAddress = "0x74C358f27E1008f1a912e65208A448B6D194228f";
+export const CrowdFundingABI = crowdFunding.abi;
